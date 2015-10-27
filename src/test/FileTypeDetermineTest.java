ea8e613cfb97bf29424be145c8423edeb59e1669
unknown <markol@10g.pl>
2015-10-27 19:09:37 +0100

    junit test for image type determining

--- src/test/FileTypeDetermineTest.java
+++ src/test/FileTypeDetermineTest.java
@@ -2,16 +2,41 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import biblioteki.FileTypeDetermine;
 
 public class FileTypeDetermineTest {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
 	public void testAudioType() {
 		String fileType = FileTypeDetermine.getFileTypeFromPath("C:\\Windows\\Media\\chord.wav");
 		assertEquals("audio/wav", fileType);
+	}
+	
+	@Test
+	public void testImageType() {
+		String fileType = FileTypeDetermine.getFileTypeFromPath("C:\\Windows\\Web\\Wallpaper\\Theme1\\img1.jpg");
+		assertEquals("image/jpeg", fileType);
+		
+		fileType = FileTypeDetermine.getFileTypeFromPath("C:\\Users\\student\\pictures\\tgaSample.tga");
+		assertEquals(null, fileType);
+		
+		fileType = FileTypeDetermine.getFileTypeFromPath("C:\\Users\\student\\pictures\\pngSample.png");
+		assertEquals("image/png", fileType);
+		
+		fileType = FileTypeDetermine.getFileTypeFromPath("C:\\Users\\student\\pictures\\gifSample.gif");
+		assertEquals("image/gif", fileType);
+		
+		//fileType = FileTypeDetermine.getFileTypeFromPath("C:\\bla.gif");
+		 //thrown.expect(FileNotFoundException.class);
 	}
 
 }
