287b7336616c85dc9a25d81e5c1a889cba6862d1
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-10-27 18:35:38 +0100

    dodanie implementacji klasy Main.java

--- bibliotekaSzyfr/bibliotekaSzyfr/Main.java
+++ bibliotekaSzyfr/bibliotekaSzyfr/Main.java
@@ -6,5 +6,14 @@ import javax.crypto.*;
 
 public class Main {
 
+	 public static void main(String[] args) throws Exception {
+	    	
+	        String FileName = "encryptedtext.txt";
+	        String FileName2 = "decryptedtext.txt";
+
+	        Szyfr.szyfrowanie(FileName, FileName2);
+	        
+	        return;
+	    }
    
 }
