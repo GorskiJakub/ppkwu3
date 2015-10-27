96d9c765b8df9c3b771c802f01b2d473bc6142c1
GorskiJakub <jakubg93@o2.pl>
2015-10-27 19:16:32 +0100

    niedokonczony test, koniec zajec

--- src/test/bibliotekiTest.java
+++ src/test/bibliotekiTest.java
@@ -3,31 +3,20 @@ package test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.junit.Test;
+
 import biblioteki.HttpFile;
 
 public class bibliotekiTest {
 
 	@Test
-	public void testHttpFileForWww() {
-		HttpFile hf = null;
-		try {
-			hf = new HttpFile(new URL("https://jarekprzygodzki.wordpress.com/"));
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			String content = new String(hf.getData(), hf.getEncoding());
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
+	public void testHttpFile() throws MalformedURLException, IOException {
+		HttpFile hf = new HttpFile(new URL("https://jarekprzygodzki.wordpress.com/"));
+		String content = new String(hf.getData(), hf.getEncoding());
 
-		assertEquals(true, hf);
+		assert
 	}
 
 }
