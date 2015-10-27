719da6babe46cf6c954cec60bb7d39654b357b8c
GorskiJakub <jakubg93@o2.pl>
2015-10-27 19:08:01 +0100

    test dla biblioteki http dla przypadku www, red, cos jeszcze nie dziala

--- src/test/bibliotekiTest.java
+++ src/test/bibliotekiTest.java
@@ -2,13 +2,32 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.junit.Test;
+import biblioteki.HttpFile;
 
 public class bibliotekiTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testHttpFileForWww() {
+		HttpFile hf = null;
+		try {
+			hf = new HttpFile(new URL("https://jarekprzygodzki.wordpress.com/"));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			String content = new String(hf.getData(), hf.getEncoding());
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(true, hf);
 	}
 
 }
